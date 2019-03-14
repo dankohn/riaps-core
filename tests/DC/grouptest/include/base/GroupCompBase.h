3e1d4d104b7759dbab03e05c2dc4c13728fe3206
istvan <istvan.madari@vanderbilt.edu>
2019-03-14 13:50:48 -0500

    base class updated

--- tests/DC/grouptest/include/base/GroupCompBase.h
+++ tests/DC/grouptest/include/base/GroupCompBase.h
@@ -6,11 +6,18 @@
 #define RIAPS_CORE_GROUPCOMP_H
 
 #include "componentmodel/r_componentbase.h"
+#include "messages/MessageType.capnp.h"
+#include "componentmodel/r_messagebuilder.h"
+#include <componentmodel/r_messagereader.h>
+
+#include <pybind11/stl.h>
+#include <pybind11/pybind11.h>
+#include <boost/optional.hpp>
+
+namespace py = pybind11;
 
 // Name of the ports from the model file
-#define PORT_TIMER_CLOCK "clock"
-#define GROUP_TYPE_GROUPA "groupA"
-#define GROUP_TYPE_GROUPB "groupB"
+constexpr auto PORT_TIMER_CLOCK  = "clock";
 
 namespace grouptest {
    namespace components {
@@ -18,14 +25,24 @@ namespace grouptest {
       class GroupCompBase : public riaps::ComponentBase {
          
          public:
-         GroupCompBase(_component_conf &config, riaps::Actor &actor);
+         GroupCompBase(const py::object *parent_actor,
+                       const py::dict actor_spec, // Actor json config
+                       const py::dict type_spec,  // component json config
+                       const std::string &name,
+                       const std::string &type_name,
+                       const py::dict args,
+                       const std::string &application_name,
+                       const std::string &actor_name);
          
-         virtual void OnClock(riaps::ports::PortBase *port)=0;
+         virtual void OnClock()=0;
+         virtual timespec RecvClock() final;
          
-         virtual ~GroupCompBase();
+         virtual ~GroupCompBase() = default;
          protected:
-         virtual void DispatchMessage(capnp::FlatArrayMessageReader* capnpreader, riaps::ports::PortBase *port,std::shared_ptr<riaps::MessageParams> params=nullptr );
-         virtual void DispatchInsideMessage(zmsg_t* zmsg, riaps::ports::PortBase* port);
+          virtual void DispatchMessage(riaps::ports::PortBase* port) final;
+
+          virtual void DispatchInsideMessage(zmsg_t* zmsg,
+                                             riaps::ports::PortBase* port) final;
       };
    }
 }
